bcf7e1c3327cbdbaec6de149caefb415ce8e553f
Thiyagarajan B <38608518+thiyagu-7@users.noreply.github.com>
2019-12-25 14:28:28 +0530

    Advent of code day 3 - part I

---
+++ src/main/java/com/thiyagu_7/adventofcode/year2019/day3/SolutionDay3.java
@@ -0,0 +1,115 @@
+package com.thiyagu_7.adventofcode.year2019.day3;
+
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+
+
+public class SolutionDay3 {
+    private static final Map<Character, BiFunction<Coordinate, Integer, Coordinate>> COORDINATE_DIRECTION;
+
+    //Current location
+    private Coordinate centralPort = new Coordinate(17500, 19500);
+    private Coordinate current = centralPort;
+    private int result = Integer.MAX_VALUE;
+
+    static {
+        COORDINATE_DIRECTION = new HashMap<>();
+        COORDINATE_DIRECTION.put('U', (c, length) -> new Coordinate(c.getX() - length, c.getY()));
+        COORDINATE_DIRECTION.put('D', (c, length) -> new Coordinate(c.getX() + length, c.getY()));
+        COORDINATE_DIRECTION.put('L', (c, length) -> new Coordinate(c.getX(), c.getY() - length));
+        COORDINATE_DIRECTION.put('R', (c, length) -> new Coordinate(c.getX(), c.getY() + length));
+    }
+
+    public int part1(String wire1Paths, String wire2Paths) {
+        List<Pair<Coordinate, Coordinate>> lineSegments = new ArrayList<>();
+        BiConsumer<Coordinate, Coordinate> addLineSegment = (start, end) -> lineSegments.add(new Pair<>(start, end));
+
+        Arrays.stream(wire1Paths.split(","))
+                .forEach(path -> processPath(path, addLineSegment));
+
+        BiConsumer<Coordinate, Coordinate> updateManhattanDistance = (start, end) -> findIntersectionAndUpdateResult(lineSegments, start, end);
+
+        //reset current back to central port
+        current = centralPort;
+        Arrays.stream(wire2Paths.split(","))
+                .forEach(path -> processPath(path, updateManhattanDistance));
+
+        return result;
+    }
+
+    private void processPath(String path,
+                             BiConsumer<Coordinate, Coordinate> lineSegmentConsumer) {
+        //System.out.println(current.getX() + " " + current.getY() + " " + path);
+        char direction = path.charAt(0);
+        int length = Integer.parseInt(path.substring(1));
+        BiFunction<Coordinate, Integer, Coordinate> directionMap = COORDINATE_DIRECTION.get(direction);
+
+        Coordinate newPoint = directionMap.apply(current, length);
+        lineSegmentConsumer.accept(current, newPoint);
+        current = newPoint;
+    }
+
+
+    //https://stackoverflow.com/questions/15726825/find-overlap-between-collinear-lines
+    private void findIntersectionAndUpdateResult(List<Pair<Coordinate, Coordinate>> wire1LineSegments,
+                                                 Coordinate start, Coordinate end) {
+        //Check for intersection
+        for(Pair<Coordinate, Coordinate> pair: wire1LineSegments) {
+            Coordinate wire1LineStart = pair.getKey();
+            Coordinate wire1LineEnd= pair.getValue();
+
+            if (wire1LineStart.getX() > wire1LineEnd.getX() || wire1LineStart.getY() > wire1LineEnd.getY()) {
+                //swap
+                Coordinate tmp = wire1LineStart;
+                wire1LineStart = wire1LineEnd;
+                wire1LineEnd = tmp;
+            }
+
+            if (start.getX() > end.getX() || start.getY() > end.getY()) {
+                //swap
+                Coordinate tmp = start;
+                start = end;
+                end = tmp;
+            }
+
+            boolean condition1 = wire1LineEnd.getX() >= start.getX() && wire1LineEnd.getY() >= start.getY();
+            boolean condition2 = wire1LineStart.getX() <= end.getX() && wire1LineStart.getY() <= end.getY();
+
+            if (condition1 && condition2) {
+                //intersection point
+                int intX = Math.max(wire1LineStart.getX(), start.getX());
+                int intY = Math.min(wire1LineEnd.getY(), end.getY());
+
+                int dist = Math.abs(centralPort.getX() - intX) + Math.abs(centralPort.getY() - intY);
+                if (dist != 0) {
+                    result = Math.min(result, dist);
+                }
+            }
+        }
+    }
+
+    private static class Coordinate {
+        private int x;
+        private int y;
+
+        private Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+    }
+}
---
+++ src/main/java/com/thiyagu_7/adventofcode/year2019/day3/SolutionTry1.java
@@ -0,0 +1,107 @@
+package com.thiyagu_7.adventofcode.year2019.day3;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
+
+//Does not work as have to allocate a big array - Out of heap space
+//Works with 8GB heap
+public class SolutionTry1 {
+    private static final Map<Character, UnaryOperator<Coordinate>> COORDINATE_DIRECTION;
+    private int[][] gridPanel = new int[35000][39000];
+    //Central port
+    private int x = gridPanel.length / 2;
+    private int y = gridPanel[0].length / 2;
+
+    //Current location
+    private Coordinate centralPort = new Coordinate(x, y);
+    private Coordinate current = new Coordinate(x, y);
+    private int result = Integer.MAX_VALUE;
+
+    static {
+        COORDINATE_DIRECTION = new HashMap<>();
+        COORDINATE_DIRECTION.put('U', c -> new Coordinate(c.getX() - 1, c.getY()));
+        COORDINATE_DIRECTION.put('D', c -> new Coordinate(c.getX() + 1, c.getY()));
+        COORDINATE_DIRECTION.put('L', c -> new Coordinate(c.getX(), c.getY() - 1));
+        COORDINATE_DIRECTION.put('R', c -> new Coordinate(c.getX(), c.getY() + 1));
+    }
+
+    public int part1(String wire1Paths, String wire2Paths) {
+        Arrays.stream(wire1Paths.split(","))
+                .forEach(path -> processPath(path, 1, Optional.empty()));
+
+        Optional<Consumer<Coordinate>> updateManhattanDistance = Optional.of(this::updateManhattanDistance);
+
+        //reset current back to central port
+        current = centralPort;
+        Arrays.stream(wire2Paths.split(","))
+                .forEach(path -> processPath(path, 2, updateManhattanDistance));
+
+        return result;
+    }
+
+    private void processPath(String path, int wireNum,
+                             Optional<Consumer<Coordinate>> coordinateConsumer) {
+        char direction = path.charAt(0);
+        int length = Integer.parseInt(path.substring(1));
+        UnaryOperator<Coordinate> directionMap = COORDINATE_DIRECTION.get(direction);
+
+        for (int i = 0; i < length; i++) {
+            current = directionMap.apply(current);
+            coordinateConsumer.ifPresent(consumer -> consumer.accept(current));
+
+            gridPanel[current.getX()][current.getY()] = wireNum;
+        }
+    }
+
+
+    private void updateManhattanDistance(Coordinate newCoordinate) {
+        //Check for intersection
+        if (gridPanel[newCoordinate.getX()][newCoordinate.getY()] == 1) {
+            System.out.println(result + " " + (Math.abs(newCoordinate.getX() - x) + Math.abs(newCoordinate.getY() - y)));
+            result = Math.min(result, Math.abs(newCoordinate.getX() - x) + Math.abs(newCoordinate.getY() - y));
+        }
+    }
+
+    private static class Coordinate {
+        private int x;
+        private int y;
+
+        private Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+    }
+
+    public static void main(String[] args) {
+        SolutionTry1 solutionTry1 = new SolutionTry1();
+        List<String> lines = readFile("/year2019/day3/input.txt");
+        System.out.println(solutionTry1.part1(lines.get(0), lines.get(1)));
+    }
+
+    public static List<String> readFile(String resourcePath) {
+        try {
+            return Files.readAllLines(Paths.get(SolutionTry1.class
+                    .getResource(resourcePath)
+                    .toURI()));
+        } catch (URISyntaxException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
