974023ac32ffaf06445161eec5b187bd81e198db
Thiyagarajan B <38608518+thiyagu-7@users.noreply.github.com>
2020-01-12 18:06:00 +0530

    Day 5 - Part II

--- src/main/java/com/thiyagu_7/adventofcode/year2019/day5/SolutionDay5.java
+++ src/main/java/com/thiyagu_7/adventofcode/year2019/day5/SolutionDay5.java
@@ -76,4 +76,88 @@ public class SolutionDay5 {
             return index; //immediate mode
         }
     }
+
+    public List<Integer> part2(int[] inputs, int valueToRead) {
+        List<Integer> resultOutputs = new ArrayList<>();
+        int index = 0;
+
+        while (inputs[index] != 99) {
+            List<Integer> digits = parseOpcodeAndParameter(inputs[index]);
+            int len = digits.size();
+            int opcode = digits.get(len - 1);
+
+            int parameterModeIndex = len - 3; //since last 2 digits are opcode
+            int result;
+            switch (opcode) {
+                case 1:
+                    int num1 = resolve(inputs, inputs[index + 1], parameterModeIndex >= 0 ? digits.get(parameterModeIndex--) : 0);
+                    int num2 = resolve(inputs, inputs[index + 2], parameterModeIndex >= 0 ? digits.get(parameterModeIndex) : 0);
+                    result = num1 + num2;
+
+                    int targetIndex = inputs[index + 3];
+                    inputs[targetIndex] = result;
+                    index += 4;
+                    break;
+                case 2:
+                    num1 = resolve(inputs, inputs[index + 1], parameterModeIndex >= 0 ? digits.get(parameterModeIndex--) : 0);
+                    num2 = resolve(inputs, inputs[index + 2], parameterModeIndex >= 0 ? digits.get(parameterModeIndex) : 0);
+                    result = num1 * num2;
+
+                    targetIndex = inputs[index + 3];
+                    inputs[targetIndex] = result;
+                    index += 4;
+                    break;
+                case 3:
+                    //Input
+                    System.out.println("READING");
+                    targetIndex = inputs[index + 1];
+                    inputs[targetIndex] = valueToRead;
+                    index += 2;
+                    break;
+                case 4:
+                    //Output
+                    int target = resolve(inputs, inputs[index + 1], parameterModeIndex >= 0 ? digits.get(parameterModeIndex) : 0);
+                    System.out.println(target);
+                    resultOutputs.add(target);
+                    index += 2;
+                    break;
+
+                case 5:
+                    int val = resolve(inputs, inputs[index + 1], parameterModeIndex >= 0 ? digits.get(parameterModeIndex--) : 0);
+                    if (val != 0) {
+                        index = resolve(inputs, inputs[index + 2], parameterModeIndex >= 0 ? digits.get(parameterModeIndex) : 0);
+                    } else {
+                        index += 3;
+                    }
+                    break;
+                case 6:
+                    val = resolve(inputs, inputs[index + 1], parameterModeIndex >= 0 ? digits.get(parameterModeIndex--) : 0);
+                    if (val == 0) {
+                        index = resolve(inputs, inputs[index + 2], parameterModeIndex >= 0 ? digits.get(parameterModeIndex) : 0);
+                    } else {
+                        index += 3;
+                    }
+                    break;
+                case 7:
+                    int param1 = resolve(inputs, inputs[index + 1], parameterModeIndex >= 0 ? digits.get(parameterModeIndex--) : 0);
+                    int param2 = resolve(inputs, inputs[index + 2], parameterModeIndex >= 0 ? digits.get(parameterModeIndex) : 0);
+
+                    //write and hence position only
+                    int param3 = inputs[index + 3];
+                    inputs[param3] = param1 < param2 ? 1 : 0;
+                    index += 4;
+                    break;
+                case 8:
+                    param1 = resolve(inputs, inputs[index + 1], parameterModeIndex >= 0 ? digits.get(parameterModeIndex--) : 0);
+                    param2 = resolve(inputs, inputs[index + 2], parameterModeIndex >= 0 ? digits.get(parameterModeIndex) : 0);
+
+                    //write and hence position only
+                    param3 = inputs[index + 3];
+                    inputs[param3] = param1 == param2 ? 1 : 0;
+                    index += 4;
+                    break;
+            }
+        }
+        return resultOutputs;
+    }
 }
--- src/test/java/com/thiyagu_7/adventofcode/year2019/day5/SolutionDay5Test.java
+++ src/test/java/com/thiyagu_7/adventofcode/year2019/day5/SolutionDay5Test.java
@@ -3,9 +3,11 @@ package com.thiyagu_7.adventofcode.year2019.day5;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static com.thiyagu_7.adventofcode.FileUtils.readFile;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class SolutionDay5Test {
@@ -21,6 +23,81 @@ public class SolutionDay5Test {
     @Test
     public void test_part1() {
         assertEquals(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0, 6761139), solutionDay5.part1(getInputs()));
+    }
+
+    @Test
+    public void test_part2_simple_equal_position_mode() {
+        int[] inputs = new int[]{3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8};
+        List<Integer> writtenValues = solutionDay5.part2(inputs, 8);
+        int[] finalInputs = new int[]{3, 9, 8, 9, 10, 9, 4, 9, 99, 1, 8};
+        assertEquals(Collections.singletonList(1), writtenValues);
+        assertArrayEquals(finalInputs, inputs);
+    }
+
+    @Test
+    public void test_part2_simple_less_than_position_mode() {
+        int[] inputs = new int[]{3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8};
+        List<Integer> writtenValues = solutionDay5.part2(inputs, 8);
+        int[] finalInputs = new int[]{3, 9, 7, 9, 10, 9, 4, 9, 99, 0, 8};
+        assertEquals(Collections.singletonList(0), writtenValues);
+        assertArrayEquals(finalInputs, inputs);
+    }
+
+    @Test
+    public void test_part2_simple_equal_immediate_mode() {
+        int[] inputs = new int[]{3, 3, 1108, -1, 8, 3, 4, 3, 99};
+        List<Integer> writtenValues = solutionDay5.part2(inputs, 8);
+        int[] finalInputs = new int[]{3, 3, 1108, 1, 8, 3, 4, 3, 99};
+        assertEquals(Collections.singletonList(1), writtenValues);
+        assertArrayEquals(finalInputs, inputs);
+    }
+
+    @Test
+    public void test_part2_simple_less_than_immediate_mode() {
+        int[] inputs = new int[]{3, 3, 1107, -1, 8, 3, 4, 3, 99};
+        List<Integer> writtenValues = solutionDay5.part2(inputs, 8);
+        int[] finalInputs = new int[]{3, 3, 1107, 0, 8, 3, 4, 3, 99};
+        assertEquals(Collections.singletonList(0), writtenValues);
+        assertArrayEquals(finalInputs, inputs);
+    }
+
+    @Test
+    public void test_part2_jump_position_mode() {
+        int[] inputs = new int[]{3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9};
+        List<Integer> writtenValues = solutionDay5.part2(inputs, 8);
+        int[] finalInputs = new int[]{3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, 8, 1, 1, 9};
+        assertEquals(Collections.singletonList(1), writtenValues);
+        assertArrayEquals(finalInputs, inputs);
+    }
+
+    @Test
+    public void test_part2_jump_immediate_mode() {
+        int[] inputs = new int[]{3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1};
+        List<Integer> writtenValues = solutionDay5.part2(inputs, 8);
+        int[] finalInputs = new int[]{3, 3, 1105, 8, 9, 1101, 0, 0, 12, 4, 12, 99, 1};
+        assertEquals(Collections.singletonList(1), writtenValues);
+        assertArrayEquals(finalInputs, inputs);
+    }
+
+    @Test
+    public void test_part2_large_example() {
+        int[] inputs = new int[]{3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+                1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+                999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99};
+
+        List<Integer> writtenValues = solutionDay5.part2(Arrays.copyOf(inputs, inputs.length), 7);
+        assertEquals(Collections.singletonList(999), writtenValues);
+        writtenValues = solutionDay5.part2(Arrays.copyOf(inputs, inputs.length), 8);
+        assertEquals(Collections.singletonList(1000), writtenValues);
+
+        writtenValues = solutionDay5.part2(Arrays.copyOf(inputs, inputs.length), 9);
+        assertEquals(Collections.singletonList(1001), writtenValues);
+    }
+
+    @Test
+    public void test_part2() {
+        //assertEquals(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0, 6761139), solutionDay5.part1(getInputs()));
+        System.out.println(solutionDay5.part2(getInputs(), 5));
     }
 
     private int[] getInputs() {
